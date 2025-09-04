c6b8b47a2de13527e87c94057072d436213902eb
pswbuild <pswbuild@oppo.com>
2025-09-04 17:47:45 +0800

    Synchronize code for OnePlus CPH2573_15.0.0.850(EX01) CPH2581_15.0.0.850(EX01) CPH2583_15.0.0.850(EX01) PJD110_15.0.0.850(CN01) Based on QCOM release TAG: kernel/build:AU_LINUX_KERNEL.PLATFORM.3.0.R1.00.00.00.017.100 kernel/devicetree:AU_LINUX_KERNEL.PLATFORM.3.0.R1.00.00.00.017.100 platform/vendor/nxp/opensource/driver:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/opensource/camera-kernel:AU_TECHPACK_CAMERA.LA.4.0.R2.00.00.00.000.066 platform/vendor/opensource/dataipa:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/opensource/display-drivers:AU_TECHPACK_DISPLAY.LA.4.0.R2.00.00.00.000.072 platform/vendor/opensource/eva-kernel:AU_TECHPACK_CV.LA.2.0.R1.00.00.00.000.048 platform/vendor/opensource/mm-drivers:AU_TECHPACK_DISPLAY.LA.4.0.R2.00.00.00.000.075 platform/vendor/opensource/mmrm-driver:AU_TECHPACK_VIDEO.LA.4.0.R2.00.00.00.000.059 platform/vendor/opensource/synx-kernel:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/opensource/video-driver:AU_TECHPACK_VIDEO.LA.4.0.R2.00.00.00.000.059 platform/vendor/proprietary/mm-devicetree:AU_TECHPACK_DISPLAY.LA.4.0.R2.00.00.00.000.075 platform/vendor/qcom-opensource/bt-kernel:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom-opensource/wlan/fw-api:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom-opensource/wlan/platform:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom-opensource/wlan/qcacld-3.0:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom-opensource/wlan/utils/sigma-dut:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom-proprietary/audio-devicetree:AU_TECHPACK_AUDIO.LA.9.0.R1.00.00.00.000.070 platform/vendor/qcom-proprietary/bt-devicetree:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom-proprietary/camera-devicetree:AU_TECHPACK_CAMERA.LA.4.0.R2.00.00.00.000.067 platform/vendor/qcom-proprietary/data-devicetree:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom-proprietary/display-devicetree:AU_TECHPACK_DISPLAY.LA.4.0.R2.00.00.00.000.075 platform/vendor/qcom-proprietary/ese-devicetree:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom-proprietary/graphics-devicetree:AU_TECHPACK_GRAPHICS.LA.14.0.R1.00.00.00.000.073 platform/vendor/qcom-proprietary/mmrm-devicetree:AU_TECHPACK_VIDEO.LA.4.0.R2.00.00.00.000.059 platform/vendor/qcom-proprietary/nfc-devicetree:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom-proprietary/video-devicetree:AU_TECHPACK_VIDEO.LA.4.0.R2.00.00.00.000.059 platform/vendor/qcom-proprietary/wlan-devicetree:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom/opensource/audio-kernel-ar:AU_TECHPACK_AUDIO.LA.9.0.R1.00.00.00.000.070 platform/vendor/qcom/opensource/datarmnet:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom/opensource/datarmnet-ext:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/qcom/opensource/dsp-kernel:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom/opensource/graphics-kernel:AU_TECHPACK_GRAPHICS.LA.14.0.R1.00.00.00.000.074 platform/vendor/qcom/opensource/securemsm-kernel:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.160 platform/vendor/qcom/opensource/spu-kernel:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/st/opensource/driver:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166 platform/vendor/st/opensource/eSE-driver:AU_LINUX_ANDROID_LA.VENDOR.14.3.0.R1.00.00.00.000.166

--- vendor/oplus/kernel/charger/v2/charger_ic/oplus_hal_adsp.h
+++ vendor/oplus/kernel/charger/v2/charger_ic/oplus_hal_adsp.h
@@ -408,6 +408,25 @@ enum WLS_BOOST_SOURCE {
 	WLS_BOOST_SOURCE_PMIC_WLS,
 };
 
+enum charging_status {
+	CHARGING_TYPE_UNKNOW,
+	CHARGING_TYPE_VOOC_SVOOC,
+	CHARGING_TYPE_OPLUS_UFCS,
+	CHARGING_TYPE_OPLUS_PPS,
+	CHARGING_TYPE_THIRD_UFCS,
+	CHARGING_TYPE_THIRD_PPS,
+	CHARGING_TYPE_FFC,
+	CHARGING_TYPE_MAX,
+};
+
+enum qbg_full_temp_region {
+	QBG_TEMP_COLD,
+	QBG_TEMP_COOL,
+	QBG_TEMP_NORMAL,
+	QBG_TEMP_WARM,
+	QBG_TEMP_MAX,
+};
+
 enum OEM_MISC_CTL_CMD {
 	OEM_MISC_CTL_CMD_LCM_EN = 0,
 	OEM_MISC_CTL_CMD_LCM_25K = 2,
@@ -740,6 +759,9 @@ struct battery_chg_dev {
 	struct delayed_work check_adspfg_status;
 	struct delayed_work hboost_notify_work;
 #endif
+	int batt_full_para[CHARGING_TYPE_MAX][QBG_TEMP_MAX];
+	int batt_full_temp[QBG_TEMP_MAX];
+	bool batt_full_method_new;
 };
 
 /**********************************************************************
